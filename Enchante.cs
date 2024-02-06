05cffe22b11d74e9adad88eec7a0b772707e3fea
Martin Laurence Coloma <cmartinlaurence@gmail.com>
2024-02-07 01:18:17 +0800

    C - 1.2.1.1.1.1.0

--- Enchante.cs
+++ Enchante.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,24 +13,28 @@ namespace Enchante
 {
     public partial class Enchante : Form
     {
+        //local db connection
+        public static string mysqlconn = "server=localhost;user=root;database=enchante;password=";
+        public MySqlConnection connection = new MySqlConnection(mysqlconn);
+
         //cardlayout panel classes
         private ParentCard ParentPanelShow; //Parent Card
-
-        //Landing Page Navbar
-        private Panel enchanteNavBar;
-        private Panel enchanteHomePage;
-
+        
+        //tool tip
+        private System.Windows.Forms.ToolTip iconToolTip;
 
         public Enchante()
         {
             InitializeComponent();
+            
             // Exit MessageBox 
             this.FormClosing += new FormClosingEventHandler(MainForm_FormClosing);
 
             //Main Form Panel Manager
-            ParentPanelShow = new ParentCard(EnchanteHomePage, EnchanteStaffPage, EnchanteMngrPage, EnchanteMemberPage);
+            ParentPanelShow = new ParentCard(EnchanteHomePage, EnchanteStaffPage, EnchanteMngrPage, EnchanteMemberPage,EnchanteAdminPage);
 
-
+            //icon tool tip
+            iconToolTip = new System.Windows.Forms.ToolTip();
 
 
 
@@ -84,6 +89,22 @@ namespace Enchante
         private void EnchanteHLoginBtn_Click(object sender, EventArgs e)
         {
 
+
+            if (EnchanteLoginForm.Visible == false)
+            {
+                // Scroll to the Home position (0, 0)
+                ScrollToCoordinates(0, 0);
+
+                EnchanteLoginForm.Visible = true;
+                return;
+            }
+            else
+            {
+                // Scroll to the Home position (0, 0)
+                ScrollToCoordinates(0, 0);
+                EnchanteLoginForm.Visible = false;
+                    
+            }
 
         }
 
@@ -172,9 +193,221 @@ namespace Enchante
             EnchanteTeamBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(221)))));
         }
 
+        private void ShowHidePassBtn_Click(object sender, EventArgs e)
+        {
+            if(LoginPassText.UseSystemPasswordChar == true)
+            {
+                LoginPassText.UseSystemPasswordChar = false;
+                ShowHidePassBtn.IconChar = FontAwesome.Sharp.IconChar.EyeSlash;
+            }
+            else if(LoginPassText.UseSystemPasswordChar == false)
+            {
+                LoginPassText.UseSystemPasswordChar = true;
+                ShowHidePassBtn.IconChar = FontAwesome.Sharp.IconChar.Eye;
+
+            }
+        }
+        private void ShowHidePassBtn_MouseHover(object sender, EventArgs e)
+        {
+            if (LoginPassText.UseSystemPasswordChar == true)
+            {
+                iconToolTip.SetToolTip(ShowHidePassBtn, "Show Password");
+            }
+            else if (LoginPassText.UseSystemPasswordChar == false)
+            {
+                iconToolTip.SetToolTip(ShowHidePassBtn, "Hide Password");
+            }
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            loginchecker();
+        }
+        private void LoginPassText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                loginchecker();
+
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void loginchecker()
+        {
+            if (LoginEmailAddText.Text == "Admin" && LoginPassText.Text == "Admin123")
+            {
+                //Test Admin
+                MessageBox.Show("Welcome back, Admin.", "Login Verified", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ParentPanelShow.PanelShow(EnchanteAdminPage);
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = false;
+
+                logincredclear();
+                return;
+            }
+            else if (LoginEmailAddText.Text != "Admin" && LoginPassText.Text == "Admin123")
+            {
+                //Test Admin
+                LoginEmailAddErrorLbl.Visible = true;
+                LoginPassErrorLbl.Visible = false;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Admin" && LoginPassText.Text != "Admin123")
+            {
+                //Test Admin
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = true;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Manager" && LoginPassText.Text == "Manager123")
+            {
+                //Test Mngr
+                MessageBox.Show("Welcome back, Manager.", "Login Verified", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ParentPanelShow.PanelShow(EnchanteMngrPage);
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = false;
+                logincredclear();
 
 
+                return;
+            }
+            else if (LoginEmailAddText.Text != "Manager" && LoginPassText.Text == "Manager123")
+            {
+                //Test Mngr
+                LoginEmailAddErrorLbl.Visible = true;
+                LoginPassErrorLbl.Visible = false;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Manager" && LoginPassText.Text != "Manager123")
+            {
+                //Test Mngr
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = true;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Staff" && LoginPassText.Text == "Staff123")
+            {
+                //Test Staff
+                MessageBox.Show("Welcome back, Staff.", "Login Verified", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ParentPanelShow.PanelShow(EnchanteStaffPage);
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = false;
+                logincredclear();
 
+                return;
+            }
+            else if (LoginEmailAddText.Text != "Staff" && LoginPassText.Text == "Staff123")
+            {
+                //Test Mngr
+                LoginEmailAddErrorLbl.Visible = true;
+                LoginPassErrorLbl.Visible = false;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Staff" && LoginPassText.Text != "Staff123")
+            {
+                //Test Mngr
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = true;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Member" && LoginPassText.Text == "Member123")
+            {
+                //Test Member
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = true;
+                MessageBox.Show("Welcome back, Member.", "Login Verified", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ParentPanelShow.PanelShow(EnchanteMemberPage);
 
+                logincredclear();
+
+                return;
+            }
+            else if (LoginEmailAddText.Text != "Member" && LoginPassText.Text == "Member123")
+            {
+                //Test Member
+                LoginEmailAddErrorLbl.Visible = true;
+                LoginPassErrorLbl.Visible = false;
+                return;
+            }
+            else if (LoginEmailAddText.Text == "Member" && LoginPassText.Text != "Member123")
+            {
+                //Test Member
+                LoginEmailAddErrorLbl.Visible = false;
+                LoginPassErrorLbl.Visible = true;
+                return;
+            }
+            else if (string.IsNullOrEmpty(LoginEmailAddText.Text) || string.IsNullOrEmpty(LoginPassText.Text))
+            {
+                MessageBox.Show("Missing text on required fields.", "Ooooops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //db connection query
+            }
+        }
+
+        private void logincredclear()
+        {
+            LoginEmailAddText.Text = "";
+            LoginPassText.Text = "";
+
+        }
+
+        private void MemberSignOut_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to sign out user?", "Sign Out Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                EnchanteLoginForm.Visible = false;
+                ParentPanelShow.PanelShow(EnchanteHomePage);
+            }
+        }
+
+        private void MngrSignOutBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to sign out user?", "Sign Out Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                EnchanteLoginForm.Visible = false;
+                ParentPanelShow.PanelShow(EnchanteHomePage);
+            }
+        }
+
+        private void StaffSignOutBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to sign out user?", "Sign Out Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                EnchanteLoginForm.Visible = false;
+                ParentPanelShow.PanelShow(EnchanteHomePage);
+            }
+        }
+
+        private void AdminSignOutBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to sign out user?", "Sign Out Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                EnchanteLoginForm.Visible = false;
+                ParentPanelShow.PanelShow(EnchanteHomePage);
+            }
+        }
+
+        private void LoginRegisterHereLbl_Click(object sender, EventArgs e)
+        {
+            //location scroll
+            int serviceSectionY = 1600;
+            ScrollToCoordinates(0, serviceSectionY);
+
+            //Change color once clicked
+            EnchanteMemberBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(183)))), ((int)(((byte)(97)))));
+            //Change back to original
+            EnchanteHomeBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(221)))));
+            EnchanteServiceBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(221)))));
+            EnchanteReviewBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(221)))));
+            EnchanteTeamBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(221)))));
+            EnchanteAbtUsBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(221)))));
+
+        }
     }
 }
